9bed41b3f756ee48167b46ef9d1eb578e806eea3
y-nakane <y-nakane@sakura.ad.jp>
2018-11-14 11:15:45 +0900

    テキストフィールドとマップビューをコードに接続

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var dispMap: MKMapView!
+    
 }
 
