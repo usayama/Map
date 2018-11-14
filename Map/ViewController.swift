7d4845c0ed11f4dfc0756d68fa623a20d536ada4
y-nakane <y-nakane@sakura.ad.jp>
2018-11-14 10:38:32 +0900

    Initial Commit

---
+++ Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by Yusuke Nakane on 2018/11/14.
+//  Copyright © 2018 usayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
